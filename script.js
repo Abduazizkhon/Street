b2168931967dc7b8813e04cd06b95ed1f8888052
Abduazizkhon <abduazizkhon.09@gmail.com>
2022-12-29 21:58:02 +0100

    First

---
+++ script.js
@@ -0,0 +1,61 @@
+function numPower(num,pow) {
+    var res=1; //return 1 for pow=0
+    for(var i=0;i<pow;i++){
+       res=res*num;
+    }
+    return res;
+  }
+  // console.log(numPower(2, 8));
+
+
+
+
+
+// console.log(numPower(100,2)); //64
+
+
+// num = 2;
+// pow = 3;
+
+// function daraja(num, pow){
+//     res = 1;
+//     for(let i = 0; i < pow; i++) {
+//         res = res * num
+//     }
+//     return res
+// }
+// console.log(daraja(3, 3));
+
+
+// let user = {
+//     name: 'John',
+//     age: 36,
+//     adress: 'Via gablete, 17',
+//     phone: '+998 99 1234567'
+// }
+// let user2 = {
+//     name: 'Nick',
+//     age: 75,
+//     adress: 'Via gablete, 17',
+//     phone: '+998 99 1234567'
+// }
+// user.name = prompt('Isim kirit');
+// console.log(user);
+
+// Yoshlani seperate and o'rta yosh topis 
+
+
+let people = {
+  user1: 15,
+  user2: 34,
+  user3: 45,
+  user4: 17,
+  user5: 9
+}
+
+
+
+
+
+
+
